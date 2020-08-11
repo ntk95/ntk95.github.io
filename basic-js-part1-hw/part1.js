0c1e81a564b1ecc5e4a8f0a7da19bc7d015ca594
ntk95 <59201502+ntk95@users.noreply.github.com>
2020-08-11 21:13:35 +0700

    Update part1.js

--- basic-js-part1-hw/part1.js
+++ basic-js-part1-hw/part1.js
@@ -1,4 +1,6 @@
 //string- bai 1
+//Viết 1 function kiểm tra 1 chuỗi có nằm trong chuỗi còn lại hay không. Nếu có trả về true, nếu không trả về false
+//Đầu vào có 2 tham số : Tham số 1 là chuỗi ban đầu, tham số 2 là chuỗi cần kiểm tra
 var str1 = "i love you";
 
 function checkchuoi(str1) {
@@ -9,6 +11,7 @@ console.log(checkchuoi(str1));
 
 
 //string - bai 2
+//Viết hàm rút ngắn chuỗi bằng cách cắt ra 8 ký tự đầu của 1 chuỗi và thêm dấu ba chấm ở cuối chuỗi. 
 var str2 = 'Xin chào các bạn';
 
 function checkStringExist(str2) {
@@ -20,6 +23,7 @@ console.log(checkStringExist(str2));
 
 
 //string - bai 3
+//Viết hàm có tác dụng biến 1 chuỗi thành chỉ viết hoa từ đầu tiên. 
 var str3 = "chÀo MừnG đẾn với techMaster";
 
 function capitalizeString(str3) {
@@ -38,6 +42,7 @@ console.log(capitalizeString(str3));
 
 
 //string - bai 4
+//Cho 1 chuỗi, hãy viết hàm có tác dụng sao chép đó chuỗi lên 10 lần.
 function lapchuoi10lan() {
     var bai4 = 'a'.repeat(10);
     return bai4;
@@ -46,6 +51,7 @@ console.log(lapchuoi10lan());
 
 
 //string - bai 5
+//Cho 1 chuỗi, hãy viết hàm có tác dụng sao chép đó chuỗi lên 10 lần, ngăn cách nhau bởi dấu gạch ngang. 
 var chuoi = 'a '.repeat(10);
 
 function lapchuoi10langachngang() {
@@ -57,6 +63,7 @@ console.log(lapchuoi10langachngang());
 
 
 //string - bai 6
+//Cho 1 chuỗi và 1 số nguyên n > 1, hãy viết hàm có tác dụng sao chép đó chuỗi lên n lần, ngăn cách nhau bởi dấu gạch ngang. 
 var n = 5;
 
 function lapchuoi_nlan() {
@@ -69,6 +76,7 @@ console.log(lapchuoi_nlan());
 
 
 //string - bai 7
+//Viết hàm đảo ngược chuỗi. Viết dunction với đầu vào là 1 chuỗi string. Trả về chuỗi đảo ngược của chuỗi đó
 function reverseString(str) {
     var splitString = str.split("");
     var reverseArray = splitString.reverse();
@@ -80,6 +88,7 @@ console.log(bai7);
 
 
 //string - bai 8
+//Cho 1 chuỗi, kiểm tra xem chuỗi đó có phải chuỗi đối xứng hay không (đọc xuôi hay ngược đều như nhau, không tính khoảng trắng, không phân biệt hoa thường), kết quả trả về true hoặc false. 
 function palindrome(str) {
     var re = /[^A-Za-z0-9]/g;
     str = str.toLowerCase().replace(re, '');
@@ -96,6 +105,7 @@ console.log(bai8);
 
 
 //array - bai 9
+//Viết hàm tìm ra số nhỏ nhất trong mảng các số. Ví dụ:
 const array1 = [2, 1, 3];
 
 function minNumber() {
@@ -105,6 +115,7 @@ function minNumber() {
 console.log(minNumber());
 
 //array - bai 10
+//Viết hàm tìm ra số lớn thứ nhì trong mảng các số. Ví dụ:
 var arr = [2, 1, 3, 4];
 
 function max2Number() {
@@ -117,6 +128,7 @@ console.log(max2Number());
 
 
 //array - bai 11
+//Viết hàm truyền vào 1 mảng tên học viên, sắp xếp lại mảng này theo chiều ngược của bảng chữ cái. Ví dụ:
 var ten = [
     'Nam', 'Hoa', 'Tuấn'
 ];
@@ -138,6 +150,7 @@ console.log(sortStudents());
 
 
 //array - bai 12
+//Tính tổng các số chia hết cho 5 từ 0 -> 100
 function tongcacsochiahet5() {
     var array = [];
     for (i = 1; i <= 100; i++) {
@@ -152,6 +165,7 @@ function tongcacsochiahet5() {
 tongcacsochiahet5(100);
 
 //array - bai 13
+//Viết hàm cho phép truyền vào 1 mảng các số, kết quả trả về là 1 mảng mới với các số là số dư tương ứng khi chia mảng cũ cho 2
 function modArrByX(arr, x) {
     var arrResult = [];
     arr.forEach(element => {
@@ -165,6 +179,7 @@ console.log(modArrByX(arr, 2));
 
 
 //array - bai 14
+//Cho 1 mảng các chuỗi. Viết hàm lọc ra các phần tử có độ dài lớn nhất.
 function longest_string(str_ara) {
     var max = str_ara[0].length;
     str_ara.map(v => max = Math.max(max, v.length));
