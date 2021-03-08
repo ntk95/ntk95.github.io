480337a3eddbee75786c19f338db7770ac2e9dcd
ntk95 <nghientimkiem95@gmail.com>
2021-03-09 01:20:30 +0700

    VITIS

---
+++ vitis/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    let isResponShow = false;
+    $(".respon-button").click(function(){
+        let display = isResponShow ? 'none':'block';
+        isResponShow = !isResponShow;
+        $(".respon-menu").css("display",display);
+    });
+});
