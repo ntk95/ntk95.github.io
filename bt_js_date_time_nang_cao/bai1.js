3c0694ba5b875108ce1278640b87db5e728f0705
{ntk95} <nghientimkiem95@gmail.com>
2021-03-23 16:01:05 +0700

    Update bai1.js

--- bt_js_date_time_nang_cao/bai1.js
+++ bt_js_date_time_nang_cao/bai1.js
@@ -1,7 +1,14 @@
 //Bài 1
 //Viết hàm getDateAgo(date, days) để hiển thị ngày trong tuần ở định dạng ngắn gọn: 
 //‘MO’, ‘TU’, ‘WE’, ‘TH’, ‘FR’, ‘SA’, ‘SU’.
-
+let date = new Date(); 
+function getWeekDay(date) {
+    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+  
+    return days[date.getDay()];
+  }
+  
+console.log("Bài 1: Hôm nay là ngày: "+ getWeekDay(date) );
 
 
 //Bài 2:
@@ -65,7 +72,31 @@ console.log("Bài 4: Còn " + getSecondsToTomorrow() + " giây nữa là đến 
 //  // yesterday's date like 31.12.16 20:00
 //alert( formatDate(new Date(new Date - 86400 * 1000)) );
 
-
+function formatDate(date) {
+    let diff = new Date() - date; 
+    if (diff < 1000) { 
+      return 'right now';
+    }
+    let sec = Math.floor(diff / 1000);
+    if (sec < 60) {
+      return sec + ' sec. ago';
+    }
+    let min = Math.floor(diff / 60000); 
+    if (min < 60) {
+      return min + ' min. ago';
+    }
+    
+    let d = date;
+    d = [
+      '0' + d.getDate(),
+      '0' + (d.getMonth() + 1),
+      '' + d.getFullYear(),
+      '0' + d.getHours(),
+      '0' + d.getMinutes()
+    ].map(component => component.slice(-2)); 
+    return d.slice(0, 3).join('.') + ' ' + d.slice(3).join(':');
+  }
+console.log("Bài 5: Hôm qua: "+ formatDate(new Date(new Date - 86400 * 1000)) );
 
 //Bài 6:
 //Viết hàm tính toán chênh lệch giữa 2 ngày bất kỳ ở dạng ( dd/mm/yyy hoặc mm/dd/yyyy). 
