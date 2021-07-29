ea012e157e281f12b731cea94742f3cea241e692
ntk95 <59201502+ntk95@users.noreply.github.com>
2021-07-29 15:53:44 +0700

    Tic tac toe

---
+++ react-hooks-tic-tac-toe-AI-bot/src/api/apiHandlers.js
@@ -0,0 +1,52 @@
+const baselineUrl = (action) =>
+		`https://d9u7x85vp9.execute-api.us-east-2.amazonaws.com/production/${action}`;
+
+export async function checkUserAuth(email) {
+	await fetch(baselineUrl('auth'), {
+		method: 'POST',
+		mode: 'cors',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(email),
+	})
+		.then((resp) => {
+			if (resp.ok) {
+				return resp.json();
+			}
+		})
+		.then((data) => {
+		sessionStorage.setItem('bearerToken', data.token);
+	})
+	.catch((err) => console.log(err));
+}
+
+export async function setMoveGetNext(boardData, setMoves, setIsXsTurn) {
+	const token = sessionStorage.getItem('bearerToken');
+
+	if (token.length) {
+		fetch(baselineUrl('engine'), {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				Authorization: `Bearer ${token}`,
+				'Content-Type': 'application/json',
+			},
+			body: boardData,
+		})
+			.then((resp) => {
+				if (resp.ok) {
+					return resp.json();
+				}
+			})
+			.then((data) => {
+				setIsXsTurn(true);
+				setMoves(data.board);
+				return data.board;
+			})
+			.catch((err) => console.log(err));
+	} else {
+ 			sessionStorage.setItem('bearerToken', null);
+	}
+
+}
---
+++ react-hooks-tic-tac-toe-AI-bot/src/components/login/LoginForm.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import './LoginForm.scss';
+
+function LoginForm({ handleAuth }) {
+	const [state, setState] = useState({
+		email: '',
+	});
+
+	const handleChange = (evt) => {
+		const value = evt.target.value;
+		setState({
+			...state,
+			[evt.target.name]: value,
+		});
+	};
+
+	return (
+		<form className='login-form' onSubmit={(evt) => handleAuth(evt, state)}>
+			<h2>Log In</h2>
+			<label>
+				Email
+				<input
+					type='email'
+					placeholder='me@memail.com'
+					name='email'
+					value={state.email}
+					onChange={handleChange}
+					required
+				/>
+			</label>
+			<br />
+			<input type='submit' value='Submit' />
+		</form>
+	);
+}
+
+export default LoginForm;
